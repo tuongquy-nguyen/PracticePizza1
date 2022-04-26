6c734f02e5344a04465c109bdfa3f0518583ee59
Nguyen Tuong Quy <tuongquynguyen.159@gmail.com>
2022-04-26 21:24:26 +0700

    Initial Commit

---
+++ PracticePizza1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticePizza1
+//
+//  Created by KET on 26/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PracticePizza1/PracticePizza1App.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePizza1App.swift
+//  PracticePizza1
+//
+//  Created by KET on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct PracticePizza1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
